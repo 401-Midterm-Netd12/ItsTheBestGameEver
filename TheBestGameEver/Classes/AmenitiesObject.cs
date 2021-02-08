092af4eed4dc7e639625f6bd6eaef12ad84dcfa4
Jordan Kidwell <jkid2023@gmail.com>
2021-02-08 13:56:44 -0800

    create method is working!

---
+++ TheBestGameEver/Classes/AmenitiesObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheBestGameEver.Classes
+{
+  public class AmenitiesObject
+  {
+    public int ID { get; set; }
+    public string Item { get; set; }
+
+  }
+}
--- TheBestGameEver/Program.cs
+++ TheBestGameEver/Program.cs
@@ -4,22 +4,28 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using TheBestGameEver.Classes;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace TheBestGameEver
 {
   class Program
   {
-    static string URL = "https://pokeapi.co/api/v2/pokemon/";
-        static void Main(string[] args)
-        {
-            start_get();
-        }
+    static HttpClient client = new HttpClient();
+    static string URL = "https://asyncinnapp.azurewebsites.net/";
+    static void Main(string[] args)
+    {
+      RunAsync();
+      start_get();
+
+    }
     // https://stackoverflow.com/questions/44775645/how-to-get-data-from-json-api-with-c-sharp-using-httpwebrequest
     // Question answered by: Keyur Patel
     // Answer provided on: Jun 27 '17 at 8:51
     private static void start_get()
     {
-      HttpWebRequest WebReq = (HttpWebRequest)WebRequest.Create(string.Format(URL));
+      HttpWebRequest WebReq = (HttpWebRequest)WebRequest.Create(string.Format(URL + "api/Amenities"));
       WebReq.Method = "GET";
       HttpWebResponse WebResp = (HttpWebResponse)WebReq.GetResponse();
       Console.WriteLine(WebResp.StatusCode);
@@ -27,30 +33,46 @@ namespace TheBestGameEver
       string jsonString;
       using (Stream stream = WebResp.GetResponseStream()) //modified from your code since the using statement disposes the stream automatically when done
       {
-          StreamReader reader = new StreamReader(stream, System.Text.Encoding.UTF8);
-          jsonString = reader.ReadToEnd();
+        StreamReader reader = new StreamReader(stream, System.Text.Encoding.UTF8);
+        jsonString = reader.ReadToEnd();
       }
-      Product items = JsonConvert.DeserializeObject<Product>(jsonString);
-      Console.WriteLine(items.Count);     //returns 921, the number of items on that page
-      Console.WriteLine(items.Next);
-      if (items.Previous == null)
+      List<AmenitiesObject> items = JsonConvert.DeserializeObject<List<AmenitiesObject>>(jsonString);
+      foreach (var amenity in items)
       {
-        Console.WriteLine("Previous = null");
-      }
-      else
-      {
-        Console.WriteLine(items.Previous);
-      }
-      foreach (var result in items.Results)
-      {
-        if (result == null)
-        {
-          Console.WriteLine("results = null");
-        }
-        Console.WriteLine(result.Name);
-        Console.WriteLine(result.Url);
+        Console.WriteLine(amenity.ID);
+        Console.WriteLine(amenity.Item);
       }
     }
-   
+    static async Task<Uri> CreateProductAsync(AmenitiesObject amenity)
+    {
+      HttpResponseMessage response = await client.PostAsJsonAsync(
+          "api/Amenities", amenity);
+      response.EnsureSuccessStatusCode();
+      return response.Headers.Location;
+    }
+    static async void RunAsync()
+    {
+      // Update port # in the following line.
+      client.BaseAddress = new Uri(URL);
+      client.DefaultRequestHeaders.Accept.Clear();
+      client.DefaultRequestHeaders.Accept.Add(
+          new MediaTypeWithQualityHeaderValue("application/json"));
+      try
+      {
+        // Create a new product
+        AmenitiesObject amenity = new AmenitiesObject
+        {
+          Item = "Gizmo"
+        };
+        var url = await CreateProductAsync(amenity);
+        Console.WriteLine($"Created at {url}");
+      }
+      catch(Exception e)
+      {
+        throw e;
+      }
+      
+        
+    }
   }
 }
