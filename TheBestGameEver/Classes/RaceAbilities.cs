62b713614136f8b58fa0ea6318f4a7acf64985f8
Jordan Kidwell <jkid2023@gmail.com>
2021-02-09 11:26:12 -0800

    added in classes for everything so far

---
+++ TheBestGameEver/Classes/RaceAbilities.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheBestGameEver.Classes
+{
+  class RaceAbilities
+  {
+    public int RaceID { get; set; }
+    public int AbilityID { get; set; }
+  }
+}
---
+++ TheBestGameEver/Classes/Roles.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheBestGameEver.Classes
+{
+  class Roles
+  {
+    public string Role { get; set; }
+  }
+}
