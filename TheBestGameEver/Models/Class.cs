d9c86b0295d7fb8f9737dbbbbb79708eee1b5576
Jordan Kidwell <jkid2023@gmail.com>
2021-02-09 17:49:49 -0800

    refactored the crud operations to accept abilities (have not hooked up directly to deployed API)

---
+++ TheBestGameEver/Models/Class.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheBestGameEver.Models
+{
+  class Class
+  {
+    public int ID { get; set; }
+    public int StatModifier { get; set; }
+    public List<ClassSkill> ClassSkills { get; set; }
+    public string ClassNames { get; set; }
+  }
+}
---
+++ TheBestGameEver/Models/ClassSkill.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheBestGameEver.Models
+{
+  class ClassSkill
+  {
+    public int ClassID { get; set; }
+    public int SkillID { get; set; }
+    public Class ClassNav { get; set; }
+    public Skill SkillNav { get; set; }
+  }
+}
